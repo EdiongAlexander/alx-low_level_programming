49f0e74769eb854c466aacbcd65018a77b2570f3
Ediong Alexander <ediongalex@gmail.com>
2023-06-21 01:20:52 +0000

    Learning C functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	const char *str = "_putchar";
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
